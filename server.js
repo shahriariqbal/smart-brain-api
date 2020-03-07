f8ccf290c8d32d10f3cb476689012b2f92e9b279
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-03-07 22:15:36 +0600

    connect front end  & server
    sign in working

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
 
 const database= {
 	users:[
@@ -28,6 +31,13 @@ const database= {
 	}
 
 
+	],
+	login:[
+      {
+      	id:'987',
+      	hash: '',
+      	email: 'john@gmail.com'
+      }
 	]
 }
 
@@ -36,6 +46,18 @@ app.get('/', (req,res)=>{
 })
 
 app.post('/signin', (req, res) => {
+
+
+bcrypt.compare("apples", '$2a$10$LBFqGMPQYSbePTsHwyb1L.UPkVaCaU3EeJMobkJHV5S5rpWbX0Ik6', function(err, res) {
+	console.log('first guess', res)
+    
+});
+bcrypt.compare("veggies", '$2a$10$LBFqGMPQYSbePTsHwyb1L.UPkVaCaU3EeJMobkJHV5S5rpWbX0Ik6' , function(err, res) {
+	console.log('second guess', res)
+
+});
+
+
  if (req.body.email === database.users[0].email   &&  req.body.password === database.users[0].password) {
  	res.json('success');
  }else{
@@ -46,6 +68,11 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req,res) => {
 	const{email, name, password} = req.body;
+
+    
+
+
+
     database.users.push({
         id: '125',
 		name: name,
@@ -88,6 +115,17 @@ app.post('/image', (req,res) => {
 		res.status(400).json('not found');
 	}
 })
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, ()=>{
  console.log('app is running ');
