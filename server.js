3d37f4ece4030012c3397d58b29c88443c70c74b
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-03-09 19:36:55 +0600

    Register done
    frontend-server-database connection done!!

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const knex = require('knex');
 
 
-const postgres = knex({
+const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
@@ -15,7 +15,9 @@ const postgres = knex({
   }
 });
 
-console.log(  postgres.select('*').from('users'));
+// db.select('*').from('users').then(data =>{
+// 	console.log(data);
+// });
 
 const app = express();
 
@@ -77,20 +79,17 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req,res) => {
 	const{email, name, password} = req.body;
 
-    
-
-
-
-    database.users.push({
-        id: '125',
-		name: name,
-		email: email,
-		
-		entries: 0,
-		joined: new Date()
+    db('users')
+        .returning('*')
+        .insert({
+    	email: email,
+    	name: name,
+    	joined: new Date()
     })
-
-    res.json(database.users[database.users.length-1]);
+     .then(user => {
+     	res.json(user[0]);
+     })
+       .catch(err => res.status(400).json('unable to register'))
 })
 
 
