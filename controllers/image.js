d98fbb23d5bf0cf0e327e8eb3dde5382ab094ac6
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-03-10 19:21:17 +0600

    Security Update
    api key move to backened

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,25 @@
+const Clarifai = require ('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '3be7451c5e3b46fdaeb108838b91a782'
+});
+
+
+const handleApiCall = (req, res) => {
+
+     app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+      .then(data => {
+      	 res.json(data);
+      })
+      .catch(err => res.status(400).json('unable to work with API'))
+
+}
+
+
+
+
+
 const handleImage= (req,res, db) => {
 	const {id } =req.body;
 
@@ -12,5 +34,6 @@ const handleImage= (req,res, db) => {
 }
 
 module.exports ={
-	handleImage
+	handleImage,
+	handleApiCall
 }
--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.post('/signin', signin.handleSignin (db, bcrypt))
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
 
 
